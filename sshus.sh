166205a15bc741ef83dd07bc3f70fdcfc09a89c9
dmi3vla <dmi3vla@gmai.com>
2025-12-13 20:32:09 +0500

    demon down restart

--- sshus.sh
+++ sshus.sh
@@ -18,12 +18,23 @@ useradd -m -G users,wheel -s /bin/bash admin
 echo "admin:admin" | chpasswd
 
 # Configure SSH to allow password authentication
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
+# Verify SSH config syntax
+sshd -t
+if [ $? -ne 0 ]; then
+    echo "SSH config error! Restoring backup..."
+    cp /etc/ssh/sshd_config.bak /etc/ssh/sshd_config
+fi
+
 # Allow admin user to use sudo without password
 echo "admin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/admin
+
+# Stop old SSH daemon if running
+/etc/init.d/sshd stop 2>/dev/null || true
 
 # Enable sshd on boot and start it (OpenRC)
 rc-update add sshd default
